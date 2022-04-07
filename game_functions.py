50293a5b9834115711cc2d2d8134d20e60c8a76b
Kirill <kirillsafin@bk.ru>
2022-04-07 20:56:39 +0300

    before refactoring

--- game_functions.py
+++ game_functions.py
@@ -1,4 +1,5 @@
 
+from os.path import exists
 from time import monotonic
 import pygame
 import sys
@@ -75,8 +76,18 @@ def update_submenu_screen(screen, ai_settings, mc, enemies, buttons):
     for enemy in enemies:
         enemy.blitme()
     # Затемняем экран игры
-    obscure_the_screen(ai_settings, screen)
+    obscure_screen(ai_settings, screen)
     # Прорисовка кнопок:
+    for button in buttons:
+        button.blitme()
+    # Обновление экрана
+    pygame.display.flip()
+
+def update_savefiles_screen(screen, buttons):
+    '''Обновляет изображение в меню файлов сохранения'''
+    # Перерисовка экрана
+    screen.fill((0, 0, 0))
+    # Прорисовка кнопок
     for button in buttons:
         button.blitme()
     # Обновление экрана
@@ -93,6 +104,23 @@ def keydown_events(event, mc, st, buttons, screen, cur_time, timer, enemies):
     elif st.state == st.SUBMENU:
         keydown_in_submenu(event, st, buttons, screen, 
         cur_time, timer, mc, enemies)
+    elif st.state == st.SAVEFILES_SAVEMODE:
+        keydown_in_savefiles(event, st, buttons, screen)
+
+def select_button(buttons, event, do_reverse = False):
+    '''Смена выбранной кнопки по нажатию кнопок вверх или вниз'''
+    upper = 0
+    lower = len(buttons) - 1
+    key_vars = {pygame.K_DOWN : ('+', lower),
+            pygame.K_UP : ('-', upper)} 
+    if do_reverse:
+        key_vars[pygame.K_DOWN], key_vars[pygame.K_UP] = \
+            key_vars[pygame.K_UP], key_vars[pygame.K_DOWN]
+    for i in range(len(buttons)):
+        if buttons[i].is_chosen and i != key_vars[event.key][1]:
+            buttons[i].is_chosen = False
+            exec('buttons[i' + key_vars[event.key][0] + '1].is_chosen = True')
+            break 
 
 def keydown_in_mainmenu(event, st, buttons):
     '''Обрабатывает нажатия клавиш в главном меню'''
@@ -140,13 +168,13 @@ def keydown_in_game(event, mc, st, buttons, screen):
         create_submenu_buttons(buttons, screen)
         st.state = st.SUBMENU
 
-def create_submenu_buttons(buttons, screen):
+def create_submenu_buttons(buttons, screen, selected_button = Button.MENU):
     '''Создает кнопки меню паузы'''
-    for i in range(3, 5):
+    for i in range(3, 6):
         buttons.append(Button(screen, i))
-        if i == Button.MENU:
+        if i == selected_button:
             buttons[i - 3].is_chosen = True
-        buttons[i - 3].rect.centery = 445 - 2 * (i - 3) * 90
+        buttons[i - 3].rect.centery = 580 - 2 * (i - 3) * 90
 
 def keydown_in_submenu(event, st, buttons, screen, 
                 cur_time, timer, mc, enemies):
@@ -179,11 +207,71 @@ def keydown_in_submenu(event, st, buttons, screen,
                     create_mainmenu_buttons(buttons, screen)
                     st.state = st.MAINMENU
                     break
+                elif buttons[i].name_number == Button.SAVE:
+                    # Переход в меню файлов сохранения
+                    buttons.clear()
+                    create_savefiles_buttons(buttons, screen)
+                    st.state = st.SAVEFILES_SAVEMODE
+                    st.pr_state = st.SUBMENU
+                    break
                 elif buttons[i].name_number == Button.EXIT:
                     sys.exit()
 
+def keydown_in_savefiles(event, st, buttons, screen):
+    '''Обрабатывает нажатия клавиш в меню файлов сохранения'''
+    if event.key == pygame.K_ESCAPE:
+        if st.pr_state == st.SUBMENU:
+            # Если предыдущим состоянием игры было меню паузы, возвращает туда
+            buttons.clear()
+            create_submenu_buttons(buttons, screen)
+            st.state = st.SUBMENU
+        elif st.pr_state == st.MAINMENU:
+            # Если пред. состоянием игры было главное меню, возвращает туда
+            buttons.clear()
+            create_mainmenu_buttons(buttons, screen)
+            st.state = st.MAINMENU
+    elif event.key == pygame.K_DOWN:
+        pass
+def create_savefiles_buttons(buttons, screen):
+    '''Создает кнопки меню сохранения файлов'''
+    for i in range(3):
+        if exists(f'save/save{i + 1}.txt'):
+            # Если файл сохранения имеется
+            buttons.append(Button(screen, Button.SAVEFILE))
+            open_savefile(i, buttons)
+        else:
+            # Если файла сохранения нет
+            buttons.append(Button(screen, Button.EMPTY))
+        if i == 2:
+            buttons[i].is_chosen = True
+        buttons[i].rect.centery = 580 - 2 * i * 90
 
+def open_savefile(number, buttons):
+    '''Открывает файл, передает данные из него кнопке'''
+    with open(f'save/save{number + 1}.txt', 'r') as f:
+        buttons[number].saved_data = f.readlines()
+        # Добавляем к изображению номер файла
+        buttons[number].image.blit(
+            pygame.image.load(f'images/Buttons/{number + 1}'), (82, 13))
+        # Перевернутая строка значения уровня
+        reversed_level = str(int(buttons[number].saved_data[0]) + 1)[::-1]
+        for i in range(len(reversed_level) + 1):
+            if i == len(reversed_level):
+                # Добавляем к изображению букву L (level)
+                buttons[number].image.blit
+                (pygame.image.load('images/Buttons/L.png'),
+                (179 - 20 * i, 13))
+            else:
+                # Добавляем к изображению одну цифру из значения уровня
+                buttons[number].image.blit(
+                    pygame.image.load(
+                        f'images/Buttons/{reversed_level[i]}.png'),
+                    (179 - 20 * i, 13))
         
+
+
+
+    
 def keyup_events(event, mc):
     '''Обрабатывает события отжатия клавиш'''
     if event.key == pygame.K_RIGHT:
@@ -225,7 +313,7 @@ def update_waves(screen, ai_settings, mc, enemies, timer, cur_time, st):
     ai_settings.waves[st.level][st.current_wave])
 
 
-def obscure_the_screen(ai_settings, screen):
+def obscure_screen(ai_settings, screen):
     '''Затемняет экран'''
     damper = pygame.Surface((ai_settings.screen_width, 
     ai_settings.screen_height))
